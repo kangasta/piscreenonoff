7a8a0f036cf3890a15be81782e3cd89e7ed32d11
Toni Kangas <toni.ar.kangas@gmail.com>
2018-11-16 15:29:37 +0200

    Add dependency info and add argparser

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # piscreenonoff
 
 Python scripts to automatically set Raspberry Pi screen on or off based on PIR motion sensor data.
+
+## Dependencies
+
+To install dependencies, run:
+
+```bash
+pip3 install pi_hcsr501 resettabletimer pigpio
+```
+
+If pigpio daemon has not already been started, run
+
+```bash
+sudo pigpiod
+```
+
+to start it.
--- piscreenonoff.py
+++ piscreenonoff.py
@@ -1,12 +1,13 @@
 from resettabletimer import ResettableTimer
 from time import sleep
+import argparse
 
 from pi_hcsr501 import HcSr501
 from screen_utils import screen
 
 class PiScreenOnOff(object):
-	def __init__(self, timeout=15*60):
-		self.__sensor = HcSr501()
+	def __init__(self, timeout=15*60, pin=17):
+		self.__sensor = HcSr501(pin)
 		self.__timer = ResettableTimer(timeout, screen, [False])
 		self.__timer.start()
 
@@ -24,6 +25,11 @@ class PiScreenOnOff(object):
 			self.__timer.cancel()
 
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--pin", type=int, default=17, help="Pin used for PIR sensor connection")
+	parser.add_argument("-t", "--timeout", type=int, default=5*60, help="Screen off timeout in seconds")
+	args = parser.parse_args()
+
 	print("Screen auto On-Off active")
-	PiScreenOnOff(60).run()
+	PiScreenOnOff(args.timeout, args.pin).run()
 	print("\rScreen auto On-Off inactive")
